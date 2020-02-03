571c34f0f723790aebb7f0f2b27951b866e08470
Askar Safipour Afshar <43677520+Askarafshar@users.noreply.github.com>
2020-02-03 00:24:56 -0600

    Update and rename HW1 to HW1.py

---
+++ Assignment1/HW1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from scipy.io import arff
+import pandas as pd
+from matplotlib import pyplot as plt
+data = arff.loadarff('cpu.with.vendor.arff')
+df = pd.DataFrame(data[0])
+
+df.head()
+
+
+df.describe()
+
+ax1 = df['MYCT'].plot.box(grid=True)
+
+ax2 = df['MMIN'].plot.box(grid=True)
+
+ax3 = df['MMAX'].plot.box(grid=True)
+
+ax4 = df['CACH'].plot.box(grid=True)
+
+ax5 = df['CHMIN'].plot.box(grid=True)
+
+ax6 = df['CHMAX'].plot.box(grid=True)
+
+ax7 = df['class'].plot.box(grid=True)
